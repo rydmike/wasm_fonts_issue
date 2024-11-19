fb180137041a15103af772617ecae7b6613c9bca
rydmike <mrydstrom+gh@gmail.com>
2024-11-20 00:15:44 +0200

    First commit, setup Flutter repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# wasm_fonts_issue
+# DRAFT: wasm_fonts_issue
+
 Repro sample to demonstrate when WASM build does not load fonts but JS build does.
---
+++ android/app/src/main/kotlin/com/example/wasm_fonts_issue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wasm_fonts_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
